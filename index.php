ce7eaa003e7f2f819b4329314bf5abcaa5f3325c
edeister <deister.ethan@student.greenriver.edu>
2024-04-02 05:33:03 -0400

    Set up error reporting, autoload.php, instantiated Base class, and created a default route.

--- index.php
+++ index.php
@@ -1,1 +1,21 @@
 <?php
+//Set error reporting to true
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+
+//Require autoload
+require_once('vendor/autoload.php');
+
+//Instantiate the Base class
+$f3 = Base::instance();
+
+//Define a default route
+$f3->route('GET /',
+    function () {
+        $view = new Template();
+        echo $view->render('views/home.html');
+    }
+);
+
+//Run fat free
+$f3->run();
