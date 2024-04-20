c5304a13530d921f25925dfe1a2fed9180baa9ae
edeister <deister.ethan@student.greenriver.edu>
2024-04-19 23:40:17 -0400

    Added htaccess, header file, and personal info file. Added routing for personal info page, as well as basic structure for info page. Added hr tags in place of bootstrap borders.

--- index.php
+++ index.php
@@ -1,21 +1,23 @@
 <?php
-//Set error reporting to true
+//Set error reporting to true, require autoload, start session
 ini_set('display_errors',1);
 error_reporting(E_ALL);
-
-//Require autoload
 require_once('vendor/autoload.php');
+session_start();
 
 //Instantiate the Base class
 $f3 = Base::instance();
 
 //Define a default route
-$f3->route('GET /',
-    function () {
-        $view = new Template();
-        echo $view->render('views/home.html');
-    }
-);
+$f3->route('GET /', function () {
+    $view = new Template();
+    echo $view->render('views/home.html');
+});
+
+$f3->route('GET /info', function () {
+    $view = new Template();
+    echo $view->render('views/personal-info.html');
+});
 
 //Run fat free
 $f3->run();
