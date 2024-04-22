8679cdf269817e106756b65e227e530d1e378428
edeister <deister.ethan@student.greenriver.edu>
2024-04-22 15:50:51 -0400

    Finished both the html and php for the personal-info page. The page currently reroutes to the experience page by default, and does not have data vallidation.

--- index.php
+++ index.php
@@ -14,7 +14,26 @@ $f3->route('GET /', function () {
     echo $view->render('views/home.html');
 });
 
-$f3->route('GET /info', function () {
+$f3->route('GET|POST /info', function ($f3) {
+
+    //If the form was submitted...
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        //TODO: Validate the data
+        //(If data is valid) Reroute to the next form page
+        $f3->reroute('views/experience.html');
+    } else {
+
+        $f3->set('SESSION.default_state', "Washington");
+        $f3->set('SESSION.states', array("Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado",
+            "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois",
+            "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts",
+            "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire",
+            "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma",
+            "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas",
+            "Utah", "Vermont", "Virginia", "West Virginia", "Wisconsin", "Wyoming"
+        ));
+    }
+
     $view = new Template();
     echo $view->render('views/personal-info.html');
 });
